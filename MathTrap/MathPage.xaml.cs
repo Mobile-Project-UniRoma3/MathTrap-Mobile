abd30cd7772de450af2a67ffe8fbbc47704265fe
Mobile-Project-UniRoma3 <douglas@speackup.org>
2022-10-11 00:53:19 +0200

    x

---
+++ MathTrap/MathPage.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MathTrap
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MathPage : ContentPage
+    {
+        private int indice = 0;
+        private double divisione = 0;
+        private long operazione = 0;
+
+        public MathPage()
+        {
+            InitializeComponent();
+        }
+
+        private void onOne(object sender, EventArgs e)
+        {
+
+        }
+
+        private void onTwo(object sender, EventArgs e)
+        {
+
+        }
+
+        private void onThree(object sender, EventArgs e)
+        {
+
+        }
+
+        private void onFour(object sender, EventArgs e)
+        {
+
+        }
+
+        private void onFive(object sender, EventArgs e)
+        {
+
+        }
+
+        private void onSix(object sender, EventArgs e)
+        {
+
+        }
+
+        private void onSeven(object sender, EventArgs e)
+        {
+
+        }
+
+        private void onEigth(object sender, EventArgs e)
+        {
+
+        }
+        private void onNine(object sender, EventArgs e)
+        {
+
+        }
+
+        private void onZero(object sender, EventArgs e)
+        {
+
+        }
+
+        private void onInvio(object sender, EventArgs e)
+        {
+
+        }
+
+        private void onCancel(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
