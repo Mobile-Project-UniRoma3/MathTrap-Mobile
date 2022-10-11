ac2d799be863a1c6c28f9c6e72eefa60b44fd1e8
Mobile-Project-UniRoma3 <douglas@speackup.org>
2022-10-11 19:59:19 +0200

    x

--- MathTrap/HelpPage.xaml.cs
+++ MathTrap/HelpPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Globalization;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -14,7 +14,23 @@ namespace MathTrap
     {
         public HelpPage()
         {
+            
             InitializeComponent();
+            
+            switch (CultureInfo.CurrentCulture.TwoLetterISOLanguageName) {
+                case "it": label1.Text = "Benvenuti in MathTrap!!";
+                           break;
+                case "fr": label1.Text = "Bienvenu en MathTrap!!"; 
+                           break;
+                case "de": label1.Text = "Willkommen zu MathTrap!!"; 
+                           break;
+                case "cn": label1.Text = "歡迎來到數學陷阱 ! !"; 
+                           break;
+                case "es": label1.Text = "¡¡Bienvenidos a MathTrap!!";
+                           break;
+                default:label1.Text = "Welcome on MathTrap!!"; 
+                        break;
+            }
         }
 
         async private void onMain(object sender, EventArgs e)
