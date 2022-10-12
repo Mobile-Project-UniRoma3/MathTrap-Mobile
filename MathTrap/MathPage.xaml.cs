633477c8bb55ef80f339d6e61c29df60bcd7d3b5
Mobile-Project-UniRoma3 <douglas@speackup.org>
2022-10-12 22:36:58 +0200

    x

--- MathTrap/MathPage.xaml.cs
+++ MathTrap/MathPage.xaml.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace MathTrap
 {
@@ -16,137 +11,175 @@ namespace MathTrap
     {
         private int index = 10;
         private int level = 0;
-        private double operator_real = 0;
-        private long operator_int = 0;
-        private string[] operators = new string[] { "+", "-", ":", "x", "dx", "int", "log", "ln", "e", "cos", "sin", "^","/","(",")" };
+        private string operator_;
+        private string[] operators = new string[] { "+", "-", ":", "x", "/", "log", "^" };
 
         public MathPage(int argc)
         {
             InitializeComponent();
-            label1.Text = "0";
-            label2.Text = "0";
-            label3.Text = "0";
-            if (argc > 0) { } else { calculetor(index, level); }
+            this.label1.Text = "0";
+            this.label2.Text = "0";
+            this.label3.Text = "0";
+            if (argc > 0) {
+            
+            } else { 
+                calculetor(this.index, this.level); 
+            }
         }
 
-        async private void calculetor(int index, int level) {
+        private void calculetor(int index, int level) {
             Random r = new Random();
-            long operator_int_A = r.Next(index, (index * 10));
-            long operator_int_B = r.Next(0, index);
+            long operator_int_A;
+            long operator_int_B;
+            string str;
+
+            if (level % 10 == 2)
+            {
+                str = operators[r.Next(4, 7)];
+                operator_int_A = r.Next(0, 11);
+                operator_int_B = r.Next(0, 10);
+            }
+            else
+            {
+                str =operators[r.Next(0, 4)];
+                operator_int_A = r.Next(index, (index * 10));
+                operator_int_B = r.Next(0, index);
+            }
 
             label1.Text = Convert.ToString(operator_int_A);                        
             label2.Text = Convert.ToString(operator_int_B);
-            if (level % 10 == 2) { }
-            else { }
-            switch (operators[r.Next(0, 4)]) {
-                case "+": operator_int = (operator_int_A + operator_int_B);
-                          break;
 
-                case "-": operator_int = (operator_int_A - operator_int_B); 
-                          break;
+            switch (str) {
+                 case "+":
+                    this.operator_ = Convert.ToString(operator_int_A + operator_int_B);
+                    break;
 
-                case ":": operator_real =(operator_int_A / operator_int_B);
-                          break;
+                 case "-":
+                    this.operator_ = Convert.ToString(operator_int_A - operator_int_B); 
+                    break;
 
-                case "x": operator_int = (operator_int_A * operator_int_B); 
-                          break;
+                 case ":":
+                    this.operator_ = Convert.ToString(operator_int_A / operator_int_B);
+                    break;
 
-                default:  operator_int = (operator_int_A ^ 2);
-                          break;
-            }
+                 case "x":
+                    this.operator_ = Convert.ToString(operator_int_A * operator_int_B); 
+                    break;
+
+                 case "/":
+                    this.label1.Text = Convert.ToString((operator_int_A) ^ operator_int_B);
+                    this.label2.Text = "1/" + Convert.ToString(operator_int_B);
+                    this.operator_ = Convert.ToString(operator_int_A);
+                    break;
+
+                 case "^":
+                    this.operator_ = Convert.ToString((operator_int_A) ^ operator_int_B);
+                    break;
+
+                 case "log":
+                    this.label1.Text = Convert.ToString(operator_int_B);
+                    this.label2.Text = Convert.ToString((operator_int_A)^operator_int_B);
+                    this.operator_ = Convert.ToString(operator_int_A);
+                    break;
+
+                 default:
+                    this.operator_ = Convert.ToString(operator_int_A ^ 2);
+                    break;
+                }
 
 
         }
-        async private void onOne(object sender, EventArgs e)
+
+        private void onOne(object sender, EventArgs e)
         {
-            if (label3.Text == "0")
-                label3.Text = "1";
+            if (this.label3.Text == "0")
+                this.label3.Text = "1";
             else
-                label3.Text += "1";
+                this.label3.Text += "1";
         }
 
-        async private void onTwo(object sender, EventArgs e)
+        private void onTwo(object sender, EventArgs e)
         {
-            if (label3.Text == "0")
-                label3.Text = "2";
+            if (this.label3.Text == "0")
+                this.label3.Text = "2";
             else
-                label3.Text += "2";
+                this.label3.Text += "2";
         }
 
-        async private void onThree(object sender, EventArgs e)
+        private void onThree(object sender, EventArgs e)
         {
-            if (label3.Text == "0")
-                label3.Text = "3";
+            if (this.label3.Text == "0")
+                this.label3.Text = "3";
             else
-                label3.Text += "3";
+                this.label3.Text += "3";
         }
 
-        async private void onFour(object sender, EventArgs e)
+        private void onFour(object sender, EventArgs e)
         {
-            if (label3.Text == "0")
-                label3.Text = "4";
+            if (this.label3.Text == "0")
+                this.label3.Text = "4";
             else
-                label3.Text += "4";
+                this.label3.Text += "4";
         }
 
-        async private void onFive(object sender, EventArgs e)
+        private void onFive(object sender, EventArgs e)
         {
-            if (label3.Text == "0")
-                label3.Text = "5";
+            if (this.label3.Text == "0")
+                this.label3.Text = "5";
             else
-                label3.Text += "5";
+                this.label3.Text += "5";
         }
 
-        async private void onSix(object sender, EventArgs e)
+        private void onSix(object sender, EventArgs e)
         {
-            if (label3.Text == "0")
-                label3.Text = "6";
+            if (this.label3.Text == "0")
+                this.label3.Text = "6";
             else
-                label3.Text += "6";
+                this.label3.Text += "6";
         }
 
-        async private void onSeven(object sender, EventArgs e)
+        private void onSeven(object sender, EventArgs e)
         {
-            if (label3.Text == "0")
-                label3.Text = "7";
+            if (this.label3.Text == "0")
+                this.label3.Text = "7";
             else
-                label3.Text += "7";
+                this.label3.Text += "7";
         }
 
-        async private void onEigth(object sender, EventArgs e)
+        private void onEigth(object sender, EventArgs e)
         {
-            if (label3.Text == "0")
-                label3.Text = "8";
+            if (this.label3.Text == "0")
+                this.label3.Text = "8";
             else
-                label3.Text += "8";
+                this.label3.Text += "8";
         }
-        async private void onNine(object sender, EventArgs e)
+        private void onNine(object sender, EventArgs e)
         {
-            if (label3.Text == "0")
-                label3.Text = "9";
+            if (this.label3.Text == "0")
+                this.label3.Text = "9";
             else
-                label3.Text += "9";
-        }
-
-        async private void onZero(object sender, EventArgs e)
-        {
-            if (label3.Text == "0")
-                label3.Text = "0";
-            else
-                label3.Text += "0";
+                this.label3.Text += "9";
         }
 
-        async private void onInvio(object sender, EventArgs e)
+        private void onZero(object sender, EventArgs e)
         {
-            if (label2.Text == label3.Text)
-                label2.Text = "1";
+            if (this.label3.Text == "0")
+                this.label3.Text = "0";
             else
-                label2.Text += "1";
+                this.label3.Text += "0";
         }
 
-        async private void onCancel(object sender, EventArgs e)
+        private void onInvio(object sender, EventArgs e)
         {
-            label3.Text = "0";
+            if (this.operator_ == this.label3.Text)
+                this.label2.Text = "1";
+            else
+                this.label2.Text += "1";
+        }
+
+        private void onCancel(object sender, EventArgs e)
+        {
+            this.label3.Text = "0";
         }
 
         async private void onExit(object sender, EventArgs e)
