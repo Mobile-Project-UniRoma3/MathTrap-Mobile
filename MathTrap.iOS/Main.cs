0e444a8a4844659ea164952aa8242742d38d46c9
Mobile-Project-UniRoma3 <douglas@speackup.org>
2022-12-21 00:29:06 +0100

    x

--- MathTrap.iOS/Main.cs
+++ MathTrap.iOS/Main.cs
@@ -10,12 +10,24 @@ namespace MathTrap.iOS
     public class Application
     {
         // This is the main entry point of the application.
-        //[Obsolete]
+        [Obsolete]
         static void Main(string[] args)
         {
-            // if you want to use a different Application Delegate class from "AppDelegate"
-            // you can specify it here.
-            UIApplication.Main(args, null, typeof(AppDelegate));
+            try {
+                // if you want to use a different Application Delegate class from "AppDelegate"
+                // you can specify it here.
+                UIApplication.Main(args, null, typeof(AppDelegate)); 
+            } catch (Exception e) {   }
         }
+
+        //public class MathTrap : UIApplication
+        //{
+        //...etc...
+        //}
+
+        //public class AppDelegate : UIApplicationDelegate
+        //{
+        //..etc...
+        //}
     }
 }
